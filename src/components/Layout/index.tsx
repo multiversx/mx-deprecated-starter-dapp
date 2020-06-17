c087065009f115905e94bb38509ff0b703925eb2
Tudor Morar <tudor.morar@elrond.com>
2020-06-17 15:19:24 +0300

    Layout markup

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,23 +1,8 @@
-import React from "react";
+import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
   <>
-    <main role="main">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      {children}
-    </main>
+    <main role="main">{children}</main>
   </>
 );
 
