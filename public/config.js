4bce34286e13b4172f65f0b1e5b66808ca3f8bf2
Tudor Morar <tudor.morar@elrond.com>
2020-06-18 15:36:58 +0300

    Config add loginCallbackUrl

--- public/config.js
+++ public/config.js
@@ -1,5 +1,7 @@
 const CONFIG = {
-  walletUrl: 'https://wallet.elrond.com/',
+  // walletUrl: 'https://wallet.elrond.com',
+  walletUrl: 'http://localhost:3001',
+  loginCallbackUrl: 'http://localhost:3000',
 };
 
 window.CONFIG = CONFIG;
