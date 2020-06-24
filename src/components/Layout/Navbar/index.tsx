f0a03abbf24d3c235f9d3da67ca4b08976c366c4
Tudor Morar <tudor.morar@elrond.com>
2020-06-24 14:59:55 +0300

    Simpligy Navbar

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
-import AppSwitcher from './AppSwitcher';
+import { Navbar, NavItem } from 'react-bootstrap';
+import { ReactComponent as ElrondLogo } from 'assets/img/elrond-symbol.svg';
 import './navbar.scss';
 
 export default function NavbarWrapper() {
   return (
     <Navbar>
       <div className="container-fluid navContainer">
-        <AppSwitcher />
-        <div className="ml-auto">
-          <p className="network mb-0">Dapp</p>
-        </div>
+        <NavItem id="basic-nav-dropdown" className="brandDropdown">
+          <span className="appSwitcherButton">
+            <ElrondLogo className="elrond-logo" />
+            <span className="activeApp">Dapp</span>
+          </span>
+        </NavItem>
       </div>
     </Navbar>
   );
