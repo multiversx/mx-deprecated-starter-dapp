bfd18a180e0099d761d77a6d0f72391e390b267a
Tudor Morar <tudor.morar@elrond.com>
2020-06-24 13:06:14 +0300

    PageState no transactions

--- src/components/Dashboard/Transactions/index.tsx
+++ src/components/Dashboard/Transactions/index.tsx
@@ -1,38 +1,22 @@
 import React from 'react';
 import { useContext } from 'context';
-import NoTransactions from './NoTransactions';
+import { ReactComponent as NoTransactionsIcon } from 'assets/img/chat-smile-3-line.svg';
 import TransactionsList from './TransactionsList';
 import { PageState } from 'sharedComponents';
 
 const Transactions = () => {
-  const { newTransactions, accountAddress } = useContext();
-  const [fristLogin, setFirstLogin] = React.useState(false);
-
-  const checkFirstLogin = () => {
-    setFirstLogin(sessionStorage.getItem('created') === accountAddress);
-    return () => {
-      sessionStorage.clear();
-    };
-  };
-
-  React.useEffect(checkFirstLogin, []);
+  const { newTransactions } = useContext();
 
   return (
     <>
       {newTransactions.length > 0 ? (
         <TransactionsList transactions={newTransactions} />
       ) : (
-        <>
-          {fristLogin ? (
-            <PageState
-              svgComponent={null}
-              title="Welcome"
-              description="No transactions found for this wallet"
-            />
-          ) : (
-            <NoTransactions />
-          )}
-        </>
+        <PageState
+          svgComponent={<NoTransactionsIcon />}
+          title="No Transactions"
+          description="No transactions found for this wallet."
+        />
       )}
     </>
   );
