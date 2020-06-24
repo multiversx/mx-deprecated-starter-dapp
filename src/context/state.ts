5a60528e5cbc9940abc9cd5d904818aaa1d96ab5
Tudor Morar <tudor.morar@elrond.com>
2020-06-24 13:06:34 +0300

    Simplify state

--- src/context/state.ts
+++ src/context/state.ts
@@ -1,44 +1,15 @@
-import { object, string, InferType } from 'yup';
+import { object, string, number, InferType } from 'yup';
 
 const configKey: any = 'CONFIG';
 const config: ConfigType = window[configKey] as any;
 
 const schema = object({
-  walletUrl: string().required(),
-  loginCallbackUrl: string().required(),
+  nodeUrl: string().required(),
+  elasticUrl: string().required(),
+  contractAddress: string().required(),
+  decimals: number().required(),
+  denomination: number().required(),
 }).defined();
-
-interface TestnetType {
-  /*
-    Legend:
-        decimals: number of displayed ERD decimals in explorer
-        denomination: number by which transaction are divided
-        numInitCharactersForScAddress: number of zeros to hide
-    Possbile flags:
-        wallet: (default) true
-        validators: (default) true
-        validatorDetails: (default) false
-        economics: (default) false
-        gasLimitEditable: (default) false
-        data: (default) false
-        faucet: (default) false (faucet)
-  */
-  default: boolean;
-  id: string;
-  name: string;
-  nodeUrl: string;
-  numInitCharactersForScAddress: number;
-  elasticUrl: string;
-  refreshRate: number;
-  decimals: number;
-  validators?: boolean;
-  denomination: number;
-  gasPrice: string;
-  gasLimit: number;
-  gasPerDataByte: number;
-  wallet?: boolean;
-  faucet: boolean;
-}
 
 export type ConfigType = InferType<typeof schema>;
 export interface TransactionType {
@@ -66,9 +37,6 @@ export interface TransactionType {
 
 export interface StateType {
   config: ConfigType;
-  activeTestnet: TestnetType;
-  metaChainShardId: number;
-  hideApp: boolean;
   accountAddress: string;
   balance: string;
   nonce: number;
@@ -77,32 +45,12 @@ export interface StateType {
   newTransactions: TransactionType[];
 }
 
-const defaultTestnet = {
-  default: false,
-  id: 'not-configured',
-  name: 'NOT CONFIGURED',
-  numInitCharactersForScAddress: 0,
-  nodeUrl: '',
-  refreshRate: 0,
-  elasticUrl: '',
-  decimals: 0,
-  denomination: 0,
-  gasPrice: '0',
-  gasLimit: 0,
-  gasPerDataByte: 0,
-  wallet: true,
-  faucet: false,
-};
-
 const initialState = (): StateType => {
   schema.validate(config, { strict: true }).catch(({ errors }) => {
     console.error('config.js format errors: ', errors);
   });
   return {
     config,
-    activeTestnet: defaultTestnet,
-    metaChainShardId: -1,
-    hideApp: false,
     accountAddress: '',
     balance: '...',
     nonce: 0,
