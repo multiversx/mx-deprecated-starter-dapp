0d5e8516ef07101da7706fcf897acc9d871550c2
Tudor Morar <tudor.morar@elrond.com>
2020-07-06 12:03:12 +0300

    Transaction fetch address from url params

--- src/components/Transaction/index.tsx
+++ src/components/Transaction/index.tsx
@@ -3,33 +3,40 @@ import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useLocation } from 'react-router-dom';
 import { PageState } from 'sharedComponents';
+import { useContext } from 'context';
 
 const Transaction = () => {
   const { search } = useLocation();
+  const { accountAddress: contextAccountAddress } = useContext();
   const query = new URLSearchParams(search);
   let explorerAddr = 'https://explorer.elrond.com/';
 
   const success = query.get('success') === 'true' ? true : false;
   const txHash = query.get('txHash');
-  // const nonce = query.get('nonce');
+  const accountAddress = query.get('accountAddress') || contextAccountAddress;
 
   return success ? (
     <PageState
       svgComponent={<FontAwesomeIcon icon={faCheck} className="text-success fa-3x" />}
       className="dapp-icon icon-medium"
       title="Transaction submitted successfully"
-      showBackBtn={true}
       description={
         <>
-          <a
-            href={`${explorerAddr}transactions/${txHash}`}
-            {...{
-              target: '_blank',
-            }}
-            className="tx-link"
-            title="View in Explorer"
-          >
-            {txHash}
+          <p>
+            <a
+              href={`${explorerAddr}transactions/${txHash}`}
+              {...{
+                target: '_blank',
+              }}
+              className="tx-link"
+              title="View in Explorer"
+            >
+              {txHash}
+            </a>
+          </p>
+
+          <a href={`/dashboard?accountAddress=${accountAddress}`} className="btn btn-primary mt-3">
+            Back to dashboard
           </a>
         </>
       }
@@ -39,8 +46,14 @@ const Transaction = () => {
       svgComponent={<FontAwesomeIcon icon={faTimes} className="text-danger fa-3x" />}
       className="dapp-icon icon-medium"
       title="Error sending transaction"
-      description="Try again"
-      showBackBtn={true}
+      description={
+        <>
+          Try again
+          <a href={`/dashboard?accountAddress=${accountAddress}`} className="btn btn-primary mt-3">
+            Back to dashboard
+          </a>
+        </>
+      }
     />
   );
 };
