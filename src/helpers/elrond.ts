02c810234bf6e6e541afa33bb86088c519ce5d7c
Tudor Morar <tudor.morar@elrond.com>
2020-07-07 11:12:28 +0300

    Make Elrond window open in center

--- src/helpers/elrond.ts
+++ src/helpers/elrond.ts
@@ -1,6 +1,8 @@
 interface ElrondType {
   window: Window | null;
-  strWindowFeatures: string;
+  windowHeight: number;
+  windowWidth: number;
+  strWindowFeatures: () => string;
   login: ({ callbackUrl }: { callbackUrl: string }) => void;
   closeWindow: () => void;
   sendTransaction: (props: {
@@ -14,15 +16,43 @@ interface ElrondType {
 
 const elrond: ElrondType = {
   window: null,
-  strWindowFeatures: 'location=yes,height=570,width=520,scrollbars=yes,status=yes',
+  windowHeight: 730,
+  windowWidth: 520,
+  strWindowFeatures: () => {
+    // Fixes dual-screen position                             Most browsers      Firefox
+    const dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : window.screenX;
+    const dualScreenTop = window.screenTop !== undefined ? window.screenTop : window.screenY;
+
+    const width = window.innerWidth
+      ? window.innerWidth
+      : document.documentElement.clientWidth
+      ? document.documentElement.clientWidth
+      : window.screen.width;
+    const height = window.innerHeight
+      ? window.innerHeight
+      : document.documentElement.clientHeight
+      ? document.documentElement.clientHeight
+      : window.screen.height;
+
+    const systemZoom = width / window.screen.availWidth;
+    const left = (width - elrond.windowWidth) / 2 / systemZoom + dualScreenLeft;
+    const top = (height - elrond.windowHeight) / 2 / systemZoom + dualScreenTop;
+    return [
+      'location=yes,scrollbars=yes,status=yes',
+      `height=${elrond.windowHeight}`,
+      `width=${elrond.windowWidth}`,
+      `top=${top}`,
+      `left=${left}`,
+    ].join(',');
+  },
   login: function ({ callbackUrl }) {
     var queryString = new URLSearchParams({
       callbackUrl,
       modal: 'true',
     });
-    // var URL = `http://localhost:3001/hook/login?${queryString}`;
-    var URL = `https://wallet.elrond.com/hook/login?${queryString}`;
-    elrond.window = window.open(URL, '_blank', elrond.strWindowFeatures);
+    var URL = `http://localhost:3001/hook/login?${queryString}`;
+    // var URL = `https://wallet.elrond.com/hook/login?${queryString}`;
+    elrond.window = window.open(URL, '_blank', elrond.strWindowFeatures());
   },
   closeWindow: () => {
     if (elrond.window) {
@@ -35,9 +65,9 @@ const elrond: ElrondType = {
       data: encodeURIComponent(props.data),
       modal: 'true',
     });
-    // var URL = `http://localhost:3001/hook/transaction?${queryString}`;
-    var URL = `https://wallet.elrond.com/hook/transaction?${queryString}`;
-    elrond.window = window.open(URL, '_blank', elrond.strWindowFeatures);
+    var URL = `http://localhost:3001/hook/transaction?${queryString}`;
+    // var URL = `https://wallet.elrond.com/hook/transaction?${queryString}`;
+    elrond.window = window.open(URL, '_blank', elrond.strWindowFeatures());
   },
 };
 
