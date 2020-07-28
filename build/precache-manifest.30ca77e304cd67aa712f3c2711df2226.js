189921b3f194b4eec0140ddb6050e7c626dcbd16
Tudor Morar <tudor.morar@elrond.com>
2020-07-28 12:08:16 +0300

    Build

---
+++ build/precache-manifest.30ca77e304cd67aa712f3c2711df2226.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90415a7b1df4103f24c4345fb90d41f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58646d3d9bf5e70afb9f",
+    "url": "/static/css/main.91099982.chunk.css"
+  },
+  {
+    "revision": "1659b8922be80c4d750c",
+    "url": "/static/js/2.e93347d8.chunk.js"
+  },
+  {
+    "revision": "d103818e803302bc4a25e2b21db0e62e",
+    "url": "/static/js/2.e93347d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58646d3d9bf5e70afb9f",
+    "url": "/static/js/main.8a6b26bf.chunk.js"
+  },
+  {
+    "revision": "d3b13bc2b1c6ac01454a",
+    "url": "/static/js/runtime-main.8b1dd952.js"
+  },
+  {
+    "revision": "477b44856e615224b2df92f0246a8c46",
+    "url": "/static/media/elrond.477b4485.svg"
+  },
+  {
+    "revision": "fc0e087c77817b6454992693ed2a96be",
+    "url": "/static/media/heart.fc0e087c.svg"
+  }
+]);
