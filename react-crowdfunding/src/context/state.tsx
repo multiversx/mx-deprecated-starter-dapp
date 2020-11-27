4fc4629ae81cd8a2d40d0bd324b8f76fe2b8b696
Cristi Corcoveanu <cristi.corcoveanu@gmail.com>
2020-11-27 14:52:07 +0200

    Make testnet wallet provider as default

--- react-crowdfunding/src/context/state.tsx
+++ react-crowdfunding/src/context/state.tsx
@@ -21,8 +21,8 @@ export const initialState = () => {
     denomination: 18,
     decimals: 2,
     dapp: {
-      //provider: new WalletProvider(WALLET_PROVIDER_TESTNET),
-      provider: new WalletProvider("https://localhost:3000/dapp/init"),
+      provider: new WalletProvider(WALLET_PROVIDER_TESTNET),
+      //provider: new WalletProvider("https://localhost:3000/dapp/init"),
       proxy: new ProxyProvider("https://testnet-api.elrond.com", 4000),
     },
     loading: false,
