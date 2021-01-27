dc8b29d11940978592fca86cf56a51966887106a
danielailie <danielailie12@gmail.com>
2021-01-27 22:47:19 +0200

    Add missing unbound function

--- react-delegationdashboard/src/components/NodesList/ActiveNodeRow.tsx
+++ react-delegationdashboard/src/components/NodesList/ActiveNodeRow.tsx
@@ -1,22 +1,48 @@
+import { Address, Argument, ContractFunction } from "@elrondnetwork/erdjs/out";
+import { Query } from "@elrondnetwork/erdjs/out/smartcontracts/query";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { useContext } from "../../context";
+import { addresses } from "../../contracts";
 import { NodeType } from "../../helpers/types";
 import Trim from "../Trim";
 import { nodeActions, nodeTransactions } from "./NodeTypes";
 
-type ActionType = 'unStake' | 'unJail' | 'unBond';
+type ActionType = 'unStake' | 'unJail' | 'unBond' | 'reStake';
 
 const allowedActions: { [key: string]: ActionType[] } = {
     staked: ['unStake'],
     jailed: ['unJail'],
     unStaked: ['unBond'],
-    queued: ['unStake'],
+    reStake: ['reStake'],
 };
 
 const ActiveNodeRow = ({ blsKey: key, index }: { blsKey: NodeType; index: number }) => {
     const { explorerAddress, dapp } = useContext();
+    const ref = React.useRef(null);
+
+    const [remaining, setRemaining] = React.useState(0);
+    const fetchUnBondPeriod = () => {
+        const query = new Query({
+            address: new Address(addresses["staking_smart_contract"]),
+            func: new ContractFunction('getRemainingUnBondPeriod'),
+            args: [Argument.fromHex(key.blsKey)]
+        })
+        if (key.status.key === 'unStaked') {
+            dapp.proxy.queryContract(query)
+                .then((value) => {
+                    const remainingUnBondPeriod = value.returnData[0].asNumber;
+                    const newRemaining = remainingUnBondPeriod !== undefined ? remainingUnBondPeriod : 0;
+                    if (ref.current !== null) {
+                        setRemaining(newRemaining * 6);
+                    }
+                })
+                .catch(e => console.log("error ", e))
+        }
+    };
+
+    React.useEffect(fetchUnBondPeriod, [key.blsKey, key.status]);
 
     const statusColor = key.status.key === 'staked' ? 'success' : key.status.key === 'jailed' ? 'danger' : 'warning';
     return (
@@ -58,7 +84,7 @@ const ActiveNodeRow = ({ blsKey: key, index }: { blsKey: NodeType; index: number
                 {Object.keys(nodeActions).map((entry) => {
                     const action: ActionType = entry as any;
                     let actionAllowed = allowedActions[key.status.key].includes(action);
-                    if (actionAllowed && action === 'unBond') {
+                    if (actionAllowed && action === 'unBond' && remaining !== 0) {
                         actionAllowed = false;
                     }
                     return (
