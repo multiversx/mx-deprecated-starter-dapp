19b3cdbac12f10cc85b761b8f5726837f6504bf6
danielailie <danielailie12@gmail.com>
2021-01-28 18:42:51 +0200

    Add new action for owner add nodes

--- react-delegationdashboard/src/components/StakeProviderActionsContainer/index.tsx
+++ react-delegationdashboard/src/components/StakeProviderActionsContainer/index.tsx
@@ -1,7 +1,10 @@
+
 import React, { useState } from "react";
 import { useContext } from "../../context";
 import { Delegation } from "../../contracts";
 import BaseActionModal from "../BaseActionModal";
+import { DropzoneFileType } from "../DropzonePem";
+import RequestVariablesModal from "../DropzonePem/RequestVariablesModal";
 
 const StakeProviderActionsContainer = () => {
     const { dapp } = useContext();
@@ -23,6 +26,26 @@ const StakeProviderActionsContainer = () => {
         const delegationContract = new Delegation(dapp.proxy, dapp.provider);
         delegationContract.sendTransaction("0", "addNodes", value).then();
     }
+
+    const getPemPubKeysWithSignature = (pemFiles: DropzoneFileType[]) => {
+        let keysData = '';
+        pemFiles.forEach(({ pubKey, signature }) => {
+          keysData += `${pubKey}@${signature}`;
+        });
+        return keysData;
+      };
+
+    const addNodesRequest = {
+        data: (pemFiles: DropzoneFileType[]) => {
+          return `${getPemPubKeysWithSignature(pemFiles)}`;
+        },
+        variables: [
+          {
+            name: 'pemFiles',
+            type: 'pemUpload',
+          },
+        ]
+      }
 
     return (
         <div className="card rounded border-3 ">
@@ -53,11 +76,16 @@ const StakeProviderActionsContainer = () => {
                         <button onClick={() => setAddNodesModal(true)} className="btn btn-primary mt-3">
                             Add nodes
                        </button>
-                        <BaseActionModal show={showAddNodes} title="Add nodes"
-                            description="Enter the number of nodes you want to add"
+                        <RequestVariablesModal
+                            name="Add nodes"
+                            show={showAddNodes}
                             handleClose={() => {
                                 setAddNodesModal(false);
-                            }} handleContinue={(value) => handleAddNodes(value)} />
+                            }}
+                            triggerDispatchEvent={(variablesData: string) => handleAddNodes(variablesData)}
+                            variables={addNodesRequest.variables}
+                            data={addNodesRequest.data}
+                        />
                     </div>
                 </div>
             </div>
