c3fdb95b4d654e637f32b76ba33345fedfd5c30c
danielailie <danielailie12@gmail.com>
2021-02-02 15:46:25 +0200

    code review follow up

---
+++ react-delegationdashboard/src/components/NodesList/StakeHooks.ts
@@ -0,0 +1,25 @@
+import { DappState } from '../../context/state';
+import { Delegation } from '../../contracts';
+
+export const nodeTransactions = {
+    unStake: (blsKey: string, dapp: DappState) => {
+        const delegationContract = new Delegation(dapp.proxy, dapp.provider);
+        return delegationContract.sendTransaction('0', 'unStakeNodes', blsKey);
+    },
+    reStake: (blsKey: string, dapp: DappState) => {
+        const delegationContract = new Delegation(dapp.proxy, dapp.provider);
+        return delegationContract.sendTransaction('0', 'reStakeUnStakedNodes', blsKey);
+    },
+    unJail: (blsKey: string, dapp: DappState) => {
+        const delegationContract = new Delegation(dapp.proxy, dapp.provider);
+        return delegationContract.sendTransaction('0', 'unJailNodes', blsKey);
+    },
+    unBond: (blsKey: string, dapp: DappState) => {
+        const delegationContract = new Delegation(dapp.proxy, dapp.provider);
+        return delegationContract.sendTransaction('0', 'unBondNodes', blsKey);
+    },
+    stake: (blsKey: string, dapp: DappState) => {
+        const delegationContract = new Delegation(dapp.proxy, dapp.provider);
+        return delegationContract.sendTransaction('0', 'stakeNodes', `${blsKey}`);
+    },
+};
