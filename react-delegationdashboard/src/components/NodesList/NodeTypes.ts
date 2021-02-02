4d38f1895f2e9d64580a1d99db4a31f75aff1446
danielailie <danielailie12@gmail.com>
2021-02-02 15:46:25 +0200

    code review follow up

---
+++ react-delegationdashboard/src/components/NodesList/NodeTypes.ts
@@ -0,0 +1,10 @@
+export const nodeActions = {
+    unJail: { label: 'Unjail', transaction: 'unJail' },
+    unStake: { label: 'Unstake', transaction: 'unStake' },
+    reStake: { label: 'ReStake', transaction: 'reSreStakeUnStakedNodestake' },
+    unBond: { label: 'Unbond', transaction: 'unBond' }
+};
+
+export const inactiveNodeActions = {
+    stake: { label: 'Stake', transaction: 'stake' },
+};
