9a86f5c827272860890209c81cb56d992dae0167
Radu Mojic <radu.mojic@elrond.com>
2021-02-05 20:13:48 +0200

    moved modules css to its respective components

--- react-delegationdashboard/src/pages/Dashboard/index.tsx
+++ react-delegationdashboard/src/pages/Dashboard/index.tsx
@@ -23,31 +23,21 @@ const Dashboard = () => {
     <div className="dashboard container py-4">
       <div className="row">
         <div className="col-12 col-md-10 mx-auto">
-          <div className="card shadow-sm rounded border-0">
-            <div className="card-body p-1">
-              <div className="card rounded mb-spacer border-0 bg-primary">
-                <div className="card-body text-center p-4">
-                  <div className="text-white">
-                    <div className="mb-1">
-                      <span className="opacity-6 mr-1">Your address:</span>
-                      <span>{address}</span>
-                    </div>
-                    <div className="mb-4">
-                      <span className="opacity-6 mr-1">Contract address:</span>
-                      <span>{delegationContract}</span>
-                    </div>
-                    <div>
-                      <h3 className="text-white">
-                        <Denominate value={balance} />
-                      </h3>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <StakeProviderArea />
-              <DelegatorArea />
+          <div className="rounded mb-spacer p-4 bg-primary text-center text-white">
+            <div className="mb-1">
+              <span className="opacity-6 mr-1">Your address:</span>
+              <span>{address}</span>
             </div>
+            <div className="mb-4">
+              <span className="opacity-6 mr-1">Contract address:</span>
+              <span>{delegationContract}</span>
+            </div>
+            <h3 className="text-white">
+              <Denominate value={balance} />
+            </h3>
           </div>
+          <StakeProviderArea />
+          <DelegatorArea />
         </div>
       </div>
     </div>
