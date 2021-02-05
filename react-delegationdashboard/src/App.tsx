f1e29c500ac65d2f5e8e2fe927d3fcc275461c6f
Radu Mojic <radu.mojic@elrond.com>
2021-02-05 19:52:13 +0200

    removed the unused file

--- react-delegationdashboard/src/App.tsx
+++ react-delegationdashboard/src/App.tsx
@@ -3,24 +3,25 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout';
 import routes from './routes';
 import { ContextProvider } from './context';
-import './App.css';
 
 function App() {
   return (
     <Router>
       <ContextProvider>
-        <Layout>
-          <Switch>
-            {routes.map((route, i) => (
-              <Route
-                path={route.path}
-                key={route.path + i}
-                component={route.component}
-                exact={true}
-              />
-            ))}
-          </Switch>
-        </Layout>
+        <Switch>
+          {routes.map((route, i) => (
+            <Route path={route.path} key={route.path + i} component={route.component} exact={true}>
+              <Layout page={route.page}>
+                <Route
+                  path={route.path}
+                  key={route.path + i}
+                  component={route.component}
+                  exact={true}
+                ></Route>{' '}
+              </Layout>
+            </Route>
+          ))}
+        </Switch>
       </ContextProvider>
     </Router>
   );
