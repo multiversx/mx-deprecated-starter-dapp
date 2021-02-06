d92c266bd793c45c2d66f234bf6a3f84aba4845e
Radu Mojic <radu.mojic@elrond.com>
2021-02-06 11:05:44 +0200

    added tabs on home

--- react-delegationdashboard/src/pages/Home/index.tsx
+++ react-delegationdashboard/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBan } from '@fortawesome/free-solid-svg-icons';
 import PageState from '../../components/PageState';
@@ -25,10 +27,25 @@ const Home = () => {
       ) : loading ? (
         <PageState svgComponent={<></>} spin />
       ) : (
-        <div className="row w-100 d-flex flex-column login__container">
-          <h1 className="text-center">Welcome to our Delegation Dashboard App</h1>
-          <LedgerLogin />
-          <WalletLogin />
+        <div className="m-auto">
+          <div className="card my-3 text-center ">
+            <div className="card-body px-0">
+              <h4 className="p-spacer">Welcome to our Delegation Dashboard App</h4>
+              <Tabs
+                defaultActiveKey="login"
+                className="login-tabs justify-content-center"
+                id="wallet-login"
+                transition={false}
+              >
+                <Tab eventKey="login" title="Ledger">
+                  <LedgerLogin />
+                </Tab>
+                <Tab eventKey="wallet" title="Wallet">
+                  <WalletLogin />
+                </Tab>
+              </Tabs>
+            </div>
+          </div>
         </div>
       )}
     </div>
