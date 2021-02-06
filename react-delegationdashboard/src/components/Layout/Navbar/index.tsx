884f868701852ba6e03d69b0bf4defb7be65d285
Radu Mojic <radu.mojic@elrond.com>
2021-02-06 17:07:31 +0200

    added admin/dashboard links in navbar

--- react-delegationdashboard/src/components/Layout/Navbar/index.tsx
+++ react-delegationdashboard/src/components/Layout/Navbar/index.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Address } from '@elrondnetwork/erdjs/out';
+import { contractViews } from 'contracts/ContractViews';
+import { Link, useLocation } from 'react-router-dom';
 import { Navbar as BsNavbar, NavItem, Nav } from 'react-bootstrap';
 import { ReactComponent as Logo } from '../../../assets/images/logo.svg';
 import { useContext, useDispatch } from '../../../context';
 
 const Navbar = () => {
-  const { loggedIn, dapp } = useContext();
+  const { loggedIn, dapp, address, delegationContract } = useContext();
   const dispatch = useDispatch();
+  const location = useLocation();
+  const { getContractConfig } = contractViews;
+  const [isAdminFlag, setIsAdminFlag] = useState(false);
+
+  useEffect(() => {
+    getContractConfig(dapp, delegationContract)
+      .then(respone => {
+        if (isAdmin(respone.returnData[0].asHex)) {
+          setIsAdminFlag(true);
+        }
+      })
+      .catch(e => console.error('getContractConfig error ', e));
+  }, []);
+
+  const isAdmin = (ownerAddress: string) => {
+    let loginAddress = new Address(address).hex();
+    return loginAddress.localeCompare(ownerAddress) < 0 ? false : true;
+  };
 
   const logOut = () => {
     dispatch({ type: 'logout', provider: dapp.provider });
@@ -20,6 +41,20 @@ const Navbar = () => {
         </NavItem>
 
         <Nav className="ml-auto">
+          {isAdminFlag && location.pathname !== '/owner' ? (
+            <Link to="/owner" className="mr-3">
+              Admin
+            </Link>
+          ) : (
+            <></>
+          )}
+          {location.pathname !== '/dashboard' ? (
+            <Link to="/dashboard" className="mr-3">
+              Dashboard
+            </Link>
+          ) : (
+            <></>
+          )}
           {loggedIn && (
             <NavItem>
               <a href="/" onClick={logOut}>
--- react-delegationdashboard/src/pages/Dashboard/index.tsx
+++ react-delegationdashboard/src/pages/Dashboard/index.tsx
@@ -1,30 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useContext } from 'context';
 import Delegator from './Delegator';
-import { Address } from '@elrondnetwork/erdjs/out';
-import { Link, Redirect } from 'react-router-dom';
-import { contractViews } from 'contracts/ContractViews';
+import { Redirect } from 'react-router-dom';
 import Header from 'components/Header';
 
 const Dashboard = () => {
-  const { address, dapp, delegationContract, loggedIn } = useContext();
-  const { getContractConfig } = contractViews;
-  const [isAdminFlag, setIsAdminFlag] = useState(false);
+  const { delegationContract, loggedIn } = useContext();
 
-  useEffect(() => {
-    getContractConfig(dapp, delegationContract)
-      .then(respone => {
-        if (isAdmin(respone.returnData[0].asHex)) {
-          setIsAdminFlag(true);
-        }
-      })
-      .catch(e => console.error('getContractConfig error ', e));
-  }, []);
-
-  const isAdmin = (ownerAddress: string) => {
-    let loginAddress = new Address(address).hex();
-    return loginAddress.localeCompare(ownerAddress) < 0 ? false : true;
-  };
   if (!loggedIn) {
     return <Redirect to="/" />;
   }
@@ -34,15 +16,7 @@ const Dashboard = () => {
         <div className="col-12 col-md-10 mx-auto card p-3">
           <div className="card-body">
             <Header />
-            {isAdminFlag ? (
-              <Link to="/owner" className="btn btn-primary">
-                View Owner Panel
-              </Link>
-            ) : (
-              <></>
-            )}
             <Delegator />
-
             <div className="card">
               <div className="card-body">
                 <span className="mr-1">Contract address:</span>
