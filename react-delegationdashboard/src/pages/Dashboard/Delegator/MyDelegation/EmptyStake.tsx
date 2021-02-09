0515cc3419e5e82611a542d92abe3be3a81822c7
danielailie <danielailie12@gmail.com>
2021-02-09 22:45:38 +0200

    Add Empty state

---
+++ react-delegationdashboard/src/pages/Dashboard/Delegator/MyDelegation/EmptyStake.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { useContext } from 'context';
+import denominate from 'components/Denominate/formatters';
+import MyActions from '../MyActions';
+import { contractViews } from 'contracts/ContractViews';
+import ClaimRewardsAction from '../MyActions/ClaimRewardsAction';
+import DelegateAction from '../MyActions/DelegateAction';
+
+const EmptyStake = () => {
+  return (
+    <div className="stats w-100 mb-spacer">
+      <div className="card">
+        <div className="card-header border-bottom-0 d-flex flex-wrap align-items-center">
+          <h6 className="mt-2 mr-2 mb-0">My Stake</h6>
+        </div>
+        <div className="card-body d-flex flex-wrap">
+          <div className="m-auto text-center py-3 py-sm-5">
+            <div>
+              <p className="m-0 text-dark">No Stake Yet</p>
+              <p className="mb-4 flex-wrap">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                Ipsum has been the industry's
+              </p>
+            </div>
+            <DelegateAction />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmptyStake;
--- react-delegationdashboard/src/pages/Dashboard/Delegator/MyDelegation/index.tsx
+++ react-delegationdashboard/src/pages/Dashboard/Delegator/MyDelegation/index.tsx
@@ -4,6 +4,7 @@ import denominate from 'components/Denominate/formatters';
 import MyActions from '../MyActions';
 import { contractViews } from 'contracts/ContractViews';
 import ClaimRewardsAction from '../MyActions/ClaimRewardsAction';
+import EmptyStake from './EmptyStake';
 
 const MyDelegation = () => {
   const { dapp, address, erdLabel, delegationContract, denomination, decimals } = useContext();
@@ -40,39 +41,46 @@ const MyDelegation = () => {
         );
       })
       .catch(e => console.error('getUserActiveStake error', e));
+    console.log('dasa', userActiveStake.localeCompare('0'));
   };
 
   React.useEffect(getAllData, []);
 
   return (
-    <div className="stats w-100 mb-spacer">
-      <div className="card">
-        <div className="card-header border-bottom-0 d-flex flex-wrap align-items-center">
-          <h6 className="mt-2 mr-2 mb-0">My Stake</h6>
-          <div className="d-flex flex-wrap align-items-center ml-sm-auto">
-            <MyActions />
+    <>
+      {userActiveStake.localeCompare('0') === 0 ? (
+        <EmptyStake />
+      ) : (
+        <div className="stats w-100 mb-spacer">
+          <div className="card">
+            <div className="card-header border-bottom-0 d-flex flex-wrap align-items-center">
+              <h6 className="mt-2 mr-2 mb-0">My Stake</h6>
+              <div className="d-flex flex-wrap align-items-center ml-sm-auto">
+                <MyActions />
+              </div>
+            </div>
+            <div className="card-body d-flex flex-wrap">
+              <div className="m-auto text-center py-3 py-sm-5">
+                <div>
+                  <p className="m-0 text-dark">Active Stake</p>
+                  <h4 className="">
+                    {userActiveStake}
+                    {erdLabel}
+                  </h4>
+                </div>
+                <div>
+                  <p className="text-muted">
+                    {claimableRewards}
+                    {erdLabel} Claimable rewards
+                  </p>
+                </div>
+                {displayRewards ? <ClaimRewardsAction /> : null}
+              </div>
+            </div>
           </div>
         </div>
-        <div className="card-body d-flex flex-wrap">
-          <div className="m-auto text-center py-3 py-sm-5">
-            <div>
-              <p className="m-0 text-dark">Active Stake</p>
-              <h4 className="">
-                {userActiveStake}
-                {erdLabel}
-              </h4>
-            </div>
-            <div>
-              <p className="text-muted">
-                {claimableRewards}
-                {erdLabel} Claimable rewards
-              </p>
-            </div>
-            {displayRewards ? <ClaimRewardsAction /> : null}
-          </div>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
