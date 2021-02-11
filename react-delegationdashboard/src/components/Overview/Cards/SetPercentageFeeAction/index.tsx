68de5b0ff138d2ced786c2136a81de4b08377697
danielailie <danielailie12@gmail.com>
2021-02-12 00:16:31 +0200

    Fix service fee

--- react-delegationdashboard/src/components/Overview/Cards/SetPercentageFeeAction/index.tsx
+++ react-delegationdashboard/src/components/Overview/Cards/SetPercentageFeeAction/index.tsx
@@ -1,26 +1,16 @@
 import { useDelegation } from 'helpers';
-import nominate from 'helpers/nominate';
 import React, { useState } from 'react';
 import SetPercentageFeeModal from './SetPercentageFeeModal';
-import BigNumber from 'bignumber.js';
 
 const SetPercentageFeeAction = () => {
   const { delegation } = useDelegation();
   const [showUpdateFeeModal, setShowUpdateFeeModal] = useState(false);
 
-  const nominateValToHex = (value: string) => {
-    let val = value && value.length > 0 ? new BigNumber(nominate(value)).toString(16) : '0';
-
-    if (val.length % 2 !== 0) {
-      val = '0' + val;
-    }
-    return val;
-  };
   const handleUpdateFee = (value: string) => {
     let perc = parseFloat(value) * 100;
     if (perc.toString().length)
       delegation
-        .sendTransaction('0', 'changeServiceFee', nominateValToHex(value).toString())
+        .sendTransaction('0', 'changeServiceFee', (parseFloat(value) * 100).toString())
         .then();
   };
 
