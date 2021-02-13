17534f4969d8344737aaa8ec3aac52fe56d38cc0
Mihai Eremia <jobs@dani.ro>
2021-02-13 22:10:33 +0000

    Remove balance header

--- react-delegationdashboard/src/components/Overview/Header/index.tsx
+++ react-delegationdashboard/src/components/Overview/Header/index.tsx
@@ -1,62 +1,33 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import denominate from 'components/Denominate/formatters';
 import { Address } from '@elrondnetwork/erdjs/out';
 import { useContext, useDispatch } from '../../../context';
-import { denomination, decimals } from 'config';
-import { getItem } from 'storage/session';
 
 const Header = () => {
   const { pathname } = useLocation();
-  const { address, delegationContract, contractOverview, account, egldLabel, dapp } = useContext();
-  const dispatch = useDispatch();
+  const { address, delegationContract, contractOverview } = useContext();
 
   const isAdmin = () => {
     let loginAddress = new Address(address).hex();
     return loginAddress.localeCompare(contractOverview.ownerAddress) === 0;
   };
 
-  const balance = denominate({
-    denomination,
-    decimals,
-    input: account.balance,
-    showLastNonZeroDecimal: false,
-  });
-
-  useEffect(() => {
-    if (balance === '...') {
-      dapp.proxy
-        .getAccount(new Address(getItem('address')))
-        .then(account => dispatch({ type: 'setBalance', balance: account.balance.toString() }));
-    }
-  }, []);
-
   return (
-    <div className="header card-header align-items-center border-0 justify-content-between">
-      <div className="d-flex border-0 align-items-center justify-content-between">
-        <div className="text-truncate">
-          <p className="opacity-6 mb-0">Contract Address</p>
-          <span className="text-truncate">{delegationContract}</span>
-        </div>
+    <div className="header card-header d-flex align-items-center border-0 justify-content-between">
+      <div className="text-truncate">
+        <p className="opacity-6 mb-0">Contract Address</p>
+        <span className="text-truncate">{delegationContract}</span>
       </div>
-      <div className="d-flex border-0 align-items-center justify-content-between">
-        <div className="text-truncate">
-          <p className="opacity-6 mb-0">Account Balance</p>
-          <span className="text-truncate">
-            {balance} {egldLabel}
-          </span>
-        </div>
-        {isAdmin() && pathname !== '/owner' ? (
-          <Link to="/owner" className="btn btn-primary btn-sm">
-            Admin
-          </Link>
-        ) : null}
-        {pathname !== '/dashboard' ? (
-          <Link to="/dashboard" className="btn btn-primary btn-sm">
-            Dashboard
-          </Link>
-        ) : null}
-      </div>
+      {isAdmin() && pathname !== '/owner' ? (
+        <Link to="/owner" className="btn btn-primary btn-sm">
+          Admin
+        </Link>
+      ) : null}
+      {pathname !== '/dashboard' ? (
+        <Link to="/dashboard" className="btn btn-primary btn-sm">
+          Dashboard
+        </Link>
+      ) : null}
     </div>
   );
 };
