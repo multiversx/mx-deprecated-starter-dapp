27b51126aec5c5bef5fc446a4f1e4fa471f53582
Mihai Eremia <jobs@dani.ro>
2021-03-01 16:05:20 +0000

    Change the delegationContract

--- react-delegationdashboard/src/config.testnet.ts
+++ react-delegationdashboard/src/config.testnet.ts
@@ -30,7 +30,7 @@ export const network: NetworkType = {
   apiAddress: 'https://testnet-api.elrond.com',
   gatewayAddress: 'https://testnet-gateway.elrond.com',
   explorerAddress: 'http://testnet-explorer.elrond.com/',
-  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqlllllskf06ky',
+  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqp0llllswfeycs',
 };
 
 const networkSchema = object({
