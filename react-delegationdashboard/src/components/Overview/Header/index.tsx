a15b73aa6849025cd973f24a1713cb099f3d869c
Mihai Eremia <jobs@dani.ro>
2021-03-03 01:40:27 +0000

    Fix comments

--- react-delegationdashboard/src/components/Overview/Header/index.tsx
+++ react-delegationdashboard/src/components/Overview/Header/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Address } from '@elrondnetwork/erdjs/out';
-import { useContext } from '../../../context';
+import { useContext } from 'context';
 import SetAgencyMetaDataModal from './SetAgencyMetaDataModal';
 
 const Header = () => {
