68431aa6cb2a331c39204a13a0416693adfa2225
danielailie <danielailie12@gmail.com>
2021-03-22 09:44:24 +0200

    Add transaction status for claim rewards also

--- react-delegationdashboard/src/pages/Dashboard/Actions/ClaimRewardsAction/ClaimRewardsModal.tsx
+++ react-delegationdashboard/src/pages/Dashboard/Actions/ClaimRewardsAction/ClaimRewardsModal.tsx
@@ -5,6 +5,8 @@ import { useDelegation } from 'helpers';
 import { useContext } from 'context';
 import BigNumber from 'bignumber.js';
 import { DelegationTransactionType } from 'helpers/contractDataDefinitions';
+import { TransactionHash } from '@elrondnetwork/erdjs/out';
+import TransactionStatusModal from 'components/LedgerTransactionStatus';
 export interface ClaimRewardsModalType {
   show: boolean;
   title: string;
@@ -15,9 +17,15 @@ const ClaimRewardsModal = ({ show, title, description, handleClose }: ClaimRewar
   const { totalActiveStake, contractOverview } = useContext();
   const [ledgerError, setLedgerDataError] = useState('');
   const [submitPressed, setSubmitPressed] = useState(false);
-
+  const [showTransactionStatus, setShowTransactionStatus] = useState(false);
+  const [txHash, setTxHash] = useState(new TransactionHash(''));
+  const displayTransactionModal = (txHash: TransactionHash) => {
+    setTxHash(txHash);
+    handleClose();
+    setShowTransactionStatus(true);
+  };
   const { sendTransaction } = useDelegation({
-    handleClose: handleClose,
+    handleClose: displayTransactionModal,
     setLedgerDataError,
     setSubmitPressed,
   });
@@ -45,40 +53,49 @@ const ClaimRewardsModal = ({ show, title, description, handleClose }: ClaimRewar
   };
 
   return (
-    <Modal show={show} onHide={handleClose} className="modal-container" animation={false} centered>
-      <div className="card">
-        <div className="card-body p-spacer text-center">
-          <p className="h6 mb-spacer" data-testid="claimRewardsTitle">
-            {title}
-          </p>
-          <p className="mb-spacer">{description}</p>
-          {ledgerError && (
-            <p className="text-danger d-flex justify-content-center align-items-center">
-              {ledgerError}
+    <>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        className="modal-container"
+        animation={false}
+        centered
+      >
+        <div className="card">
+          <div className="card-body p-spacer text-center">
+            <p className="h6 mb-spacer" data-testid="claimRewardsTitle">
+              {title}
             </p>
-          )}
-          <div className="d-flex justify-content-center align-items-center flex-wrap">
-            <ViewStatAction
-              actionTitle="Claim Rewards"
-              handleContinue={handleClaimRewards}
-              submitPressed={submitPressed}
-              color="primary"
-            />
-            {isRedelegateEnable() && (
+            <p className="mb-spacer">{description}</p>
+            {ledgerError && (
+              <p className="text-danger d-flex justify-content-center align-items-center">
+                {ledgerError}
+              </p>
+            )}
+            <div className="d-flex justify-content-center align-items-center flex-wrap">
               <ViewStatAction
-                actionTitle="Redelegate Rewards"
-                handleContinue={handleRedelegateRewards}
+                actionTitle="Claim Rewards"
+                handleContinue={handleClaimRewards}
                 submitPressed={submitPressed}
                 color="primary"
               />
-            )}
+              {isRedelegateEnable() && (
+                <ViewStatAction
+                  actionTitle="Redelegate Rewards"
+                  handleContinue={handleRedelegateRewards}
+                  submitPressed={submitPressed}
+                  color="primary"
+                />
+              )}
+            </div>
+            <button id="closeButton" className="btn btn-link mt-spacer mx-2" onClick={handleClose}>
+              Close
+            </button>
           </div>
-          <button id="closeButton" className="btn btn-link mt-spacer mx-2" onClick={handleClose}>
-            Close
-          </button>
         </div>
-      </div>
-    </Modal>
+      </Modal>
+      <TransactionStatusModal show={showTransactionStatus} txHash={txHash} />
+    </>
   );
 };
 
