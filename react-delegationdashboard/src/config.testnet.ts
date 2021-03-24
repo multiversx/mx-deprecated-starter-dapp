761c746c8707e439ad78619f265a8915b7306c45
LunaMoonfangs <nicu.sighencea@yahoo.com>
2021-03-24 10:29:53 +0200

    am modificat smart contract pt TEST NET

--- react-delegationdashboard/src/config.testnet.ts
+++ react-delegationdashboard/src/config.testnet.ts
@@ -33,7 +33,7 @@ export const network: NetworkType = {
   apiAddress: 'https://testnet-api.elrond.com',
   gatewayAddress: 'https://testnet-gateway.elrond.com',
   explorerAddress: 'http://testnet-explorer.elrond.com/',
-  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqp0llllswfeycs',
+  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqglllllsa8xtfx',
 };
 
 const networkSchema = object({
