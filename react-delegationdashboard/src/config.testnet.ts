7613d86b7df3f26fbbf4069a9f03f61b539e1191
Julien Bouteloup <julien.bouteloup@gmail.com>
2021-03-26 11:36:28 +0100

    update config testnet
    erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqk0llllsm6h64t

--- react-delegationdashboard/src/config.testnet.ts
+++ react-delegationdashboard/src/config.testnet.ts
@@ -36,7 +36,7 @@ export const network: NetworkType = {
   apiAddress: 'https://testnet-api.elrond.com',
   gatewayAddress: 'https://testnet-gateway.elrond.com',
   explorerAddress: 'http://testnet-explorer.elrond.com/',
-  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqp0llllswfeycs',
+  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqk0llllsm6h64t',
 };
 
 const networkSchema = object({
