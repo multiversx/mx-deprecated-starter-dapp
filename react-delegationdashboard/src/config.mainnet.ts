ee00577b35b38327ce2b2ac1405354dfd1cc7f47
Julien Bouteloup <julien.bouteloup@gmail.com>
2021-03-26 17:36:50 +0100

    Add mainnet address
    erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqq20llllsh0uuxv

--- react-delegationdashboard/src/config.mainnet.ts
+++ react-delegationdashboard/src/config.mainnet.ts
@@ -36,7 +36,7 @@ export const network: NetworkType = {
   apiAddress: 'https://api.elrond.com',
   gatewayAddress: 'https://gateway.elrond.com',
   explorerAddress: 'https://explorer.elrond.com/',
-  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhllllsajxzat',
+  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqq20llllsh0uuxv',
 };
 
 const networkSchema = object({
